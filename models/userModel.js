6de358b8d26fa6568e2fd3ff5eaa163e78f9ca1a
SukhNoor666 <noorchahal2004@gmail.com>
2025-12-08 10:07:39 -0700

    Update

--- models/userModel.js
+++ models/userModel.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
 
-
-const userSchema = new mongooose.Schema({
-  first_name: { type: String, required: true},
-  username: {type: String, required: true, unique: true},
-  email: {type: String, required: true, unique: true}
+//define user schema
+const userSchema = new mongoose.Schema({
+    first_name: {type: String, required: true},
+    username: {type: String, required: true},
+    email: {type: String, required: true, unique: true} 
 });
 
+//creating the User model from the schema
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
+
+
